4a521dd4d6f9b44060f3c0ad08a07f2af5065c38
Ajay Kumar <ajaykumar767580@gmail.com>
2023-01-10 11:15:28 +0530

    pushing to github

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
+
+
 router.get('/',homeController.home);
+router.use('/users', require('./users'));
+
+//for any further routes, access from here
+// router.use('/routerName', require('./routerfile'));
 
 module.exports = router;
